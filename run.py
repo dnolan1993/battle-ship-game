d0511d778f005c7240faaf1a53e058f46d15d69a
dnolan1993 <dnolan1993@yahoo.ie>
2022-09-07 20:11:57 +0000

    Add create_ships function with name of functions to be built to allow it to function correctly

--- run.py
+++ run.py
@@ -24,3 +24,17 @@ def print_board(board, opponent=False):
         row_number += 1
 
 
+def create_ships(board, ships):
+    """
+    For a given board place the number of ships required
+    """
+    for i in ships:
+        valid_placement = False
+        while not valid_placement:
+            row, col = get_empty_coordinate(board)
+            coordinates = generate_ship_coordinates(i, row, col)
+            if check_valid_placement(board, coordinates):
+                valid_placement = True
+
+        board = place_ship_on_board(board, coordinates)
+    return board
