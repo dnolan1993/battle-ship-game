21b3a08fffcc8ac4b99cff562c5a8b5f3658272f
dnolan1993 <dnolan1993@yahoo.ie>
2022-09-07 20:06:19 +0000

    Add print_board function to run.py file

--- run.py
+++ run.py
@@ -1,1 +1,26 @@
 from random import randint
+
+def print_board(board, opponent=False):
+    """
+    For a given board print it to the terminal
+    """
+    if opponent:
+        print("---------Opponents Board----------")
+    else:
+        print("-----------Your Board-------------")
+    print("    A   B   C   D   E   F   G   H")
+    print("----------------------------------")
+
+    row_number = 1
+    for row in board:
+        col_str = f"{row_number} |"
+        for col in row:
+            if opponent and col == "#":
+                val = " "
+            else:
+                val = col
+            col_str += f" {val} |"
+        print(col_str)
+        row_number += 1
+
+
